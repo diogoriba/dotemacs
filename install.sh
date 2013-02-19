36a25aa8746ba27547ef64f54b82f2a6ccbeea6f
EC2 Default User <ec2-user@ip-10-252-148-104.sa-east-1.compute.internal>
2013-02-19 19:10:57 +0000

    removing fsharp mode (defective on submodule initialization), adding flymake mode, updating install scripts not to need sudo rights

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 set +v
 # Install script for linux
-id -u | grep -e "^0$">/dev/null || { echo "You need super user rights to run this script"; exit 1; }
+#id -u | grep -e "^0$">/dev/null || { echo "You need super user rights to run this script"; exit 1; }
 command -v rsync>/dev/null || { echo "You need to install rsync to run this script"; exit 1; }
 echo Initializing git submodules
 git submodule init
@@ -16,8 +16,8 @@ rm -rf putty
 rm -rf win32
 rm ~/diogoriba.config.elc
 rm ~/.emacs.elc
-rm -rf .elisp/*.elc
-sudo rsync -prv --chmod=ugo+rwx,Dugo+rwx --exclude-from rsync.ignore . ~/ || { echo "Error copying files"; }
+rm -rf ~/.elisp/*.elc
+rsync -prv --chmod=ugo+rwx,Dugo+rwx --exclude-from rsync.ignore . ~/ || { echo "Error copying files"; }
 echo Cleaning rsync ignore file
 rm rsync.ignore
 exit 0
